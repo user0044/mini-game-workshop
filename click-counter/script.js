0ebdd71565ab199ee27dc17a90e1d19d6d605ee6
user0044 <mickeywassana0445@gmail.com>
2026-04-05 22:14:00 +0700

    Update script.js

--- click-counter/script.js
+++ click-counter/script.js
@@ -1,10 +1,8 @@
 let score = 0;
 
 function addScore(){
-score = score + 1;
-
-document
-.getElementById("score")
-.innerText = score;
-
+  score = score + 1;
+  document
+    .getElementById("score")
+    .innerText = score;
 }
