abf1cfb9c83f0f75e98a9e7a393db88e25972536
user0044 <mickeywassana0445@gmail.com>
2026-04-05 22:29:40 +0700

    Update script.js

--- guess-number/script.js
+++ guess-number/script.js
@@ -1,25 +1,16 @@
-let answer =
-Math.floor(Math.random()*10)+1;
+const result = document.getElementById("result");
+const guessInput = document.getElementById("guess");
+const answer = Math.floor(Math.random() * 10) + 1;
 
-function check(){
-  let userGuess =
-    document
-    .getElementById("guess")
-    .value;
-  if(userGuess == answer){
-    document
-    .getElementById("result")
-    .innerText = "Correct";
+function check() {
+  const userGuess = Number(guessInput.value);
+  if (userGuess === answer) {
+    result.textContent = "Correct";
+  } 
+  else if (userGuess > answer) {
+    result.textContent = "Too high";
+  } 
+  else {
+    result.textContent = "Too low";
   }
-    
-else if(userGuess > answer){
-  document
-  .getElementById("result")
-  .innerText = "Too high";
-}
-else{
-  document
-  .getElementById("result")
-  .innerText = "Too low";
-}
 }
